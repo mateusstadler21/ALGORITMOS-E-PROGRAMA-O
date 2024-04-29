e05199c04d025b07eba449d8df229705bd6d5d34
Mateus Stadler <167122798+mateusstadler21@users.noreply.github.com>
2024-04-29 15:39:41 -0300

    Exercicio 1

--- main.c
+++ main.c
@@ -2,15 +2,10 @@
 #include <stdlib.h>
 
 int main()
-{  float Celsius , Fahrenheit;
-
-   printf("Digite a temperatura em Celsus:");
-   scanf("%f" , &Celsius);
-
-   Fahrenheit = (Celsius * 9/5) + 32;
-
-   printf("O resulta da conversao e: %f\n" , Fahrenheit );
-
+{  int numero , idade;
+   printf("Digite sua idade:");
+   scanf ("%d", &numero);
+   idade =numero ;
+   printf("A sua idade e: %d\n" , idade);
    return 0;
-
 }
