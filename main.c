d3ed49a51f1762fff39ea7e87400799fedb6cd2d
Mateus Stadler <167122798+mateusstadler21@users.noreply.github.com>
2024-04-29 15:38:34 -0300

    Exercicio 2

--- main.c
+++ main.c
@@ -1,37 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main()
-{
-    //aqui fala sobre o que e o codigo ou seja comentarios
-    int numero;
-    float numeroReal;
-    char caractere;
-    double numeroPreciso;
+{  float Celsius , Fahrenheit;
 
-    //o igual é quando a variavel recebe o valor,
-    //const float PI = 3.14; //variavel constante,outra maneira de incluir uma biblioteca o #define
-    printf("\nNumero inteiro: %i" , numero);
-    scanf("%i",  &numero);
-    scanf("%f",  &numeroReal);
-    setbuf(stdin, NULL); //limpar buffer
-    scanf("%c",  &caractere);
-    scanf("%lf", &numeroPreciso);
+   printf("Digite a temperatura em Celsus:");
+   scanf("%f" , &Celsius);
 
-    printf("\nNumero inteiro: %i" , numero);
-    printf("\nnumero real: %f" , numero);
-    printf("\ncaractere: %c" , caractere);
-    printf("\nnumero preciso: %lf" , numeroPreciso);
+   Fahrenheit = (Celsius * 9/5) + 32;
 
-    numero = 10;
-    numeroPreciso = 10.5;
-    caractere = 'B';
-    numeroReal = 5.11;
+   printf("O resulta da conversao e: %f\n" , Fahrenheit );
 
-    printf("\nNumero inteiro: %i" , numero);
-    printf("\nnumero real: %f" , numero);
-    printf("\ncaractere: %c" , caractere);
-    printf("\nnumero preciso: %lf" , numeroPreciso);
+   return 0;
 
 }
